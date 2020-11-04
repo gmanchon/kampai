d4a7fbe9394e17999fd494841fa67583cb485462
Gaetan Manchon <gmanchon@gmail.com>
2020-11-04 17:59:12 +0100

    reintegrate tmp work

---
+++ kampai/templates/pred/KAMPAI_PACKAGE_SLUG/app.py
@@ -0,0 +1,14 @@
+
+# from KAMPAI_PACKAGE_NAME.trainer import Trainer
+
+
+# class App:
+
+#     def fetch(self, nrows):
+#         pass
+
+#     def head(self, nrows):
+#         pass
+
+#     def train(self):
+#         pass
