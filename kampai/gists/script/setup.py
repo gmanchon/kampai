ad9933936236d5252164b01f7f651e3f0900d11c
Gaetan Manchon <gmanchon@gmail.com>
2020-11-11 15:32:00 +0100

    add script gist

---
+++ kampai/gists/script/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    content = f.readlines()
+requirements = [x.strip() for x in content]
+
+setup(name="KAMPAI_PACKAGE_NAME",
+      version="1.0",
+      description="KAMPAI_PACKAGE_DESCRIPTION",
+      packages=find_packages(),
+      install_requires=requirements,
+      include_package_data=True,
+      scripts=["scripts/KAMPAI_PACKAGE_NAME"])
